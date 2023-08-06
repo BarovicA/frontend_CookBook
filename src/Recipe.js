ca0c21e2c4eb005c22f3cfeff506e972c7c7529e
MiladinKi <miladinkovacevic12@gmail.com>
2023-08-06 21:22:08 +0200

    Dodate CRUD operacije za Recipe
    Pored dodatih CRUD operacija, izmenjeni su i Cook, index i App.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import recipeImage from './recipe.jpg';
+import { useLoaderData } from 'react-router';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/base';
+import { async } from 'q';
+import ShowRecipe from './RecipeShow';
+import RecipeShow from './RecipeShow';
 
 const Recipe = () => {
+  const[recipes, setRecipes] = useState([]);
+
+  useEffect(() =>{
+    let ignore = false;
+    const fetchRecipe = async ()=>{
+      let r = await fetch("http://localhost:8080/api/v1/recipes");
+      let rr = await r.json();
+      if(!ignore){
+        setRecipes(rr);
+      }
+    }
+    fetchRecipe();
+    return()=>{
+      ignore = true;
+    }
+  }, []);
+  
+
+  console.log(recipes);
+  
+  const[search, setSearch] = useState("");
+
+  const filterRecipe = recipes.filter((r) => r.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className="recipe">
       <div style={{ border: '20px solid green', borderRadius: '30px', overflow: 'hidden' }}>
         <img src={recipeImage} alt="Recipe" style={{ width: '100%' }} />
       </div>
-      <h2>Naziv recepta</h2>
-      <p>Sastojci:</p>
-      <ul>
-        <li>Sastojka 1</li>
-        <li>Sastojka 2</li>
-        
-      </ul>
-      <p>Postupak:</p>
-      <p>Ovde ide opis postupka za pripremu recepta.</p>
+      <TextField size='small' id='outlined-search' label="Pretraga po nazivu recepta" type='search' 
+      sx={{backgroundColor:"whitesmoke"}}
+      onChange={(e)=>setSearch(e.target.value.toLowerCase())}/>
+      <Button size="normal" variant="contained" sx={{backgroundColor:"whitesmoke"}}>Search</Button>
+      {filterRecipe.map((r) => <RecipeShow recipe={r}/>)}
+      
     </div>
   );
 };
